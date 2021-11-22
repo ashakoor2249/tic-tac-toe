9783707bf8e58dd2de1c390ad9db6c3fb4f4ee8f
Ali Shakoor <ali.shakoor2249@gmail.com>
2021-11-22 10:45:44 -0500

    A tic tac toe game played in the command line.

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a tic tac toe game played in the command line. There are two files that make the program work.
+Player.py and game.py
+
+Player.py defines four classes, the Player class, HumanPlayer, RandomCOmputerPlayer, and SmartComputerPlayer.
---
+++ game.py
@@ -0,0 +1,160 @@
+import math
+import time
+from player import HumanPlayer, RandomComputerPlayer, SmartComputerPlayer
+
+from enum import IntEnum
+
+class Action(IntEnum):
+    computer = 0
+    smart_computer = 1
+    simulation=2
+
+def get_user_choice():
+    choices = [f"{action.name}[{action.value}]" for action in Action]
+    choices_str=",".join(choices)
+    selection=int(input(f"Enter a choice to either play tic tac toe against the computer, play against a 'smart' computer, or simulate 10 games between the computer and the 'smart' computer. ({choices_str}): "))
+    action=Action(selection)
+    return action
+
+class TicTacToe():
+    def __init__(self):
+        self.board = self.make_board()
+        self.current_winner = None
+
+    @staticmethod
+    def make_board():
+        return [' ' for _ in range(9)]
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def make_move(self, square, letter):
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
+
+    def winner(self, square, letter):
+        # check the row
+        row_ind = math.floor(square / 3)
+        row = self.board[row_ind*3:(row_ind+1)*3]
+        # print('row', row)
+        if all([s == letter for s in row]):
+            return True
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        # print('col', column)
+        if all([s == letter for s in column]):
+            return True
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            # print('diag1', diagonal1)
+            if all([s == letter for s in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            # print('diag2', diagonal2)
+            if all([s == letter for s in diagonal2]):
+                return True
+        return False
+
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def available_moves(self):
+        return [i for i, x in enumerate(self.board) if x == " "]
+
+
+def play(game, x_player, o_player, print_game=True):
+
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X'
+    while game.empty_squares():
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+        if game.make_move(square, letter):
+
+            if print_game:
+                print(letter + ' makes a move to square {}'.format(square))
+                game.print_board()
+                print('')
+
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+                return letter  # ends the loop and exits the game
+            letter = 'O' if letter == 'X' else 'X'  # switches player
+
+
+    if print_game:
+        print('It\'s a tie!')
+
+
+def main():
+    while True:
+        try:
+            user_choice = get_user_choice()
+        except ValueError as e:
+            range_str = f"[0, {len(Action) - 1}]"
+            print(f"Invalid selection. Enter a value in range {range_str}")
+            continue
+    
+        if user_choice==Action.computer:
+            o_player = HumanPlayer('O')
+            x_player = RandomComputerPlayer('X')
+            t = TicTacToe()
+            play(t, o_player, x_player, print_game=True)
+            
+
+        elif user_choice==Action.smart_computer:
+            o_player = HumanPlayer('O')
+            x_player = SmartComputerPlayer('X')
+            t = TicTacToe()
+            play(t, o_player, x_player, print_game=True)
+        
+        else:
+            x_wins=0
+            o_wins=0
+            ties=0
+            for _ in range(10):
+                x_player=RandomComputerPlayer('X')
+                o_player=SmartComputerPlayer('O')
+                t=TicTacToe()
+                result=play(t, x_player, o_player, print_game=False)
+                if result=='X':
+                    x_wins+=1
+                elif result=='O':
+                    o_wins+=1
+                else:
+                    ties+=1
+            print(f"After 10 iterations, we see {x_wins} X wins, {o_wins} O wins, {ties} ties.")
+
+        keep_going=input("Do yo want to play again? (y/n): ")
+        if keep_going.lower() !="y":
+            break
+
+# __name__
+if __name__=="__main__":
+    main()
+   
+
+
+
+
+   
